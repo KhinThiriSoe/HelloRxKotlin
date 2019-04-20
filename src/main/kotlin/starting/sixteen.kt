00a4cb983c61cc8dc0d52863a413f2ce7986ccf6
Khin <khinthirisoe.mdy@gmail.com>
2019-04-20 22:32:38 +0700

    add elementAt

--- src/main/kotlin/starting/sixteen.kt
+++ src/main/kotlin/starting/sixteen.kt
@@ -6,6 +6,9 @@ import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.rxkotlin.subscribeBy
 import io.reactivex.subjects.PublishSubject
 import landOfDroids
+import liveLongAndProsper
+import mayTheForce
+import mayTheOdds
 import wookieWorld
 
 fun main(args: Array<String>) {
@@ -30,4 +33,23 @@ fun main(args: Array<String>) {
     }
 
 
+    exampleOf("elementAt") {
+
+        val subscriptions = CompositeDisposable()
+
+        val quotes = PublishSubject.create<String>()
+
+        subscriptions.add(
+                quotes.elementAt(2) //  Return a Maybe, subscribe with onSuccess instead of onNext
+                        .subscribeBy(
+                                onSuccess = { println(it) },
+                                onComplete = { println("Completed") },
+                                onError = { println("Error ${it.localizedMessage}") }
+                        )
+        )
+
+        quotes.onNext(mayTheOdds)
+        quotes.onNext(liveLongAndProsper)
+        quotes.onNext(mayTheForce)
+    }
 }
