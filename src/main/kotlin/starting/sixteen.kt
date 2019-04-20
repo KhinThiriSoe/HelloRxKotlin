490b4f501c07b22b22aef1566fd7382c16e46a47
Khin <khinthirisoe.mdy@gmail.com>
2019-04-20 22:36:06 +0700

    add filter

--- src/main/kotlin/starting/sixteen.kt
+++ src/main/kotlin/starting/sixteen.kt
@@ -2,13 +2,17 @@ package starting
 
 import detours
 import exampleOf
+import io.reactivex.Observable
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.Observables
+import io.reactivex.rxkotlin.addTo
 import io.reactivex.rxkotlin.subscribeBy
 import io.reactivex.subjects.PublishSubject
 import landOfDroids
 import liveLongAndProsper
 import mayTheForce
 import mayTheOdds
+import tomatometerRatings
 import wookieWorld
 
 fun main(args: Array<String>) {
@@ -51,5 +55,18 @@ fun main(args: Array<String>) {
         quotes.onNext(mayTheOdds)
         quotes.onNext(liveLongAndProsper)
         quotes.onNext(mayTheForce)
+    }
+
+    exampleOf("Filter") {
+
+        val subscriptions = CompositeDisposable()
+
+        Observable.fromIterable(tomatometerRatings)
+                .filter {
+                    it.rating >= 90
+                }
+                .subscribeBy {
+                    println(it)
+                }.addTo(subscriptions)
     }
 }
